5da1bb2a078fd982b634ed50c0deb13e6e5ceecb
Luís Henrique <luishsilva09@gmail.com>
2023-11-22 20:13:10 -0300

    desafio castelos

---
+++ Castelo.js
@@ -0,0 +1,32 @@
+const terras = [1, 2, 2, 2, 3, 4, 4, 2];
+let anteriorDiferente = undefined;
+let castelo = 0;
+
+function castelos(terras) {
+  for (let i = 0; i < terras.length; i++) {
+    if (terras[i] != terras[i - 1]) {
+      anteriorDiferente = terras[i - 1];
+    }
+
+    if (terras[i] == terras[i + 1] && i != terras.length) {
+    } else if (
+      // vales
+      (anteriorDiferente > terras[i] ||
+        i == 0 ||
+        anteriorDiferente == undefined) &&
+      (terras[i + 1] > terras[i] || terras[i + 1] == undefined)
+    ) {
+      castelo++;
+    } else if (
+      // montanhas
+      (anteriorDiferente < terras[i] || i == 0) &&
+      (terras[i + 1] < terras[i] || terras[i + 1] == undefined)
+    ) {
+      castelo++;
+    }
+  }
+  return "castelos " + castelo;
+}
+
+console.log(castelos(terras));
+//21 97691 7564
