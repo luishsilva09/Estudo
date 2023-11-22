017b0ab32db09c75ffb09bb91c07ee23e3c70620
Luís Henrique <luishsilva09@gmail.com>
2023-11-22 20:22:49 -0300

    comentarios

--- Castelo.js
+++ Castelo.js
@@ -1,20 +1,21 @@
-const terras = [1, 2, 2, 2, 3, 4, 4, 2];
+const terras = [-3, -3];
 let anteriorDiferente = undefined;
 let castelo = 0;
 
 function castelos(terras) {
   for (let i = 0; i < terras.length; i++) {
+    // verifico se meu anterior é igual ao atual para savar o anterior que é diferente
     if (terras[i] != terras[i - 1]) {
       anteriorDiferente = terras[i - 1];
     }
-
+    //para caso meu promixo for igual nao fazer nada
     if (terras[i] == terras[i + 1] && i != terras.length) {
     } else if (
       // vales
       (anteriorDiferente > terras[i] ||
         i == 0 ||
         anteriorDiferente == undefined) &&
-      (terras[i + 1] > terras[i] || terras[i + 1] == undefined)
+      (terras[i + 1] > terras[i] || terras[i + 1] == undefined) // para quando estiver no final e for undefined o proximo elemento
     ) {
       castelo++;
     } else if (
